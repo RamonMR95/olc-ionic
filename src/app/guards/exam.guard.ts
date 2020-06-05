b535979ef782568d1d137049f8bbcde09ebdda1b
Ramon Moñino <ramonmr16@gmail.com>
2020-06-05 08:50:35 +0200

    Added exam guard.

---
+++ src/app/guards/exam.guard.ts
@@ -0,0 +1,56 @@
+import { Injectable, OnInit } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  CanActivate,
+  ActivatedRoute,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { ExamService } from "../services/exam.service";
+import { catchError } from "rxjs/operators";
+import { HttpErrorResponse } from "@angular/common/http";
+import { resolve } from "url";
+import { Router } from "@angular/router";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ExamGuard implements CanActivate {
+  userId: number;
+  examId: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private examService: ExamService,
+    private router: Router
+  ) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+
+    this.userId = parseInt(localStorage.getItem("id"));
+    this.examId = parseInt(route.params.id);
+    console.log(this.userId, this.examId);
+
+    return new Promise<any>((resolve, reject) => {
+      this.examService
+        .getUserExamByUserId(this.userId, this.examId)
+        .then((_) => {
+          resolve(false);
+          this.router.navigate(["/folder/Inbox"])
+        })
+        .catch((_) => {
+          resolve(true);
+        });
+    });
+  }
+
+}
