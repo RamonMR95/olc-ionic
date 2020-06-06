a1fc2fa9c5ca95e3c5b44f74cb208f9598e72e13
Antonio Ruiz Marin <Antonio507murcia@gmail.com>
2020-06-06 17:12:35 +0200

    Add children for profile, userDetails

---
+++ src/app/pages/profile/user/user.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { UserPageRoutingModule } from './user-routing.module';
+
+import { UserPage } from './user.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    UserPageRoutingModule
+  ],
+  declarations: [UserPage]
+})
+export class UserPageModule {}
---
+++ src/app/pages/profile/user/user.page.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { User } from "src/app/models/user.model";
+import { UserService } from "../../../services/user.service";
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+
+@Component({
+  selector: "app-user",
+  templateUrl: "./user.page.html",
+  styleUrls: ["./user.page.scss"],
+})
+export class UserPage implements OnInit {
+  constructor(
+    private form: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  private userDetails: FormGroup;
+  private user: User;
+  private idUser: number = parseInt(this.router.url.replace(/[^0-9]/g, ''));
+
+  ngOnInit() {
+    this.initForm();
+    this.getDataUser();
+  }
+
+  initForm() {
+    this.userDetails = this.form.group({
+      nameUser: { value: "", disabled: true},
+      surName: { value: "",disabled: true },
+      email: { value: "", disabled: true},
+      birthDate: { value: "", disabled: true},
+      description: {value: "",disabled: true}
+    });
+  }
+
+  async getDataUser() {
+    await this.userService.getUser(this.idUser).then(usr => this.user = usr);
+    this.loadUserData();
+  }
+
+  loadUserData() {
+    this.userDetails.controls.nameUser.setValue(this.user.name);
+    this.userDetails.controls.surName.setValue(this.user.surName);
+    this.userDetails.controls.email.setValue(this.user.email);
+    this.userDetails.controls.birthDate.setValue(this.user.birthDate );
+    this.userDetails.controls.description.setValue(this.user.about || "");
+  }
+
+}
